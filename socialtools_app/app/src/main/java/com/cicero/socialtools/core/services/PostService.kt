85a8ed1c3be150fe93c38091ccd5765e88db9470
rizqa febryan prastyo <47109047+cicero78M@users.noreply.github.com>
2025-07-03 23:24:32 +0700

    Add console loading demo

--- socialtools_app/app/src/main/java/com/cicero/socialtools/core/services/PostService.kt
+++ socialtools_app/app/src/main/java/com/cicero/socialtools/core/services/PostService.kt
@@ -9,6 +9,7 @@ import android.os.IBinder
 import androidx.core.app.NotificationCompat
 import android.util.Log
 import com.cicero.socialtools.R
+import com.cicero.socialtools.utils.ConsoleLoading
 import kotlinx.coroutines.*
 
 class PostService : Service() {
@@ -40,6 +41,7 @@ class PostService : Service() {
     }
 
     private suspend fun runAutopost() {
+        ConsoleLoading.showLoading(tag = "PostService")
         val delays = listOf(1000L, 2000L, 3000L)
         for ((index, d) in delays.withIndex()) {
             Log.d("PostService", "Process ${index + 1} delay: ${d}ms")
