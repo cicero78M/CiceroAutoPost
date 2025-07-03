80fb48f2c434f6b7aecc1d69c526103588e0de9b
rizqa febryan prastyo <47109047+cicero78M@users.noreply.github.com>
2025-07-03 20:58:47 +0700

    Refactor MainActivity without ViewPager

--- socialtools_app/app/src/main/java/com/cicero/socialtools/MainActivity.kt
+++ socialtools_app/app/src/main/java/com/cicero/socialtools/MainActivity.kt
@@ -4,10 +4,6 @@ import android.os.Bundle
 import android.content.Intent
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
-import androidx.viewpager2.widget.ViewPager2
-import com.google.android.material.bottomnavigation.BottomNavigationView
-import androidx.fragment.app.Fragment
-import androidx.viewpager2.adapter.FragmentStateAdapter
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,41 +11,11 @@ class MainActivity : AppCompatActivity() {
         startPostService()
         setContentView(R.layout.activity_main)
 
-        val fragments = listOf<Fragment>(
-            InstaOauthLoginFragment(),
-            InstagramToolsFragment()
-        )
-
-        val viewPager = findViewById<ViewPager2>(R.id.view_pager)
-        viewPager.adapter = object : FragmentStateAdapter(this) {
-            override fun getItemCount() = fragments.size
-            override fun createFragment(position: Int) = fragments[position]
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, InstagramToolsFragment())
+                .commit()
         }
-        viewPager.isUserInputEnabled = false
-
-        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_navigation)
-        bottomNav.setOnItemSelectedListener { item ->
-            viewPager.currentItem = when (item.itemId) {
-                R.id.nav_insta_login -> 0
-                R.id.nav_instagram_tools -> 1
-                else -> 0
-            }
-            true
-        }
-
-        viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
-            override fun onPageSelected(position: Int) {
-                val selected = when (position) {
-                    0 -> R.id.nav_insta_login
-                    else -> R.id.nav_instagram_tools
-                }
-                if (bottomNav.selectedItemId != selected) {
-                    bottomNav.selectedItemId = selected
-                }
-            }
-        })
-
-        bottomNav.selectedItemId = R.id.nav_insta_login
     }
 
     private fun startPostService() {
