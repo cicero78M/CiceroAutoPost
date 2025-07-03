d48e9b8ff9d5d4e38b73d287162a3e575ffc4e79
rizqa febryan prastyo <47109047+cicero78M@users.noreply.github.com>
2025-07-03 12:40:37 +0700

    Add flare target Instagram usernames

--- socialtools_app/app/src/main/java/com/cicero/socialtools/InstaFragment.kt
+++ socialtools_app/app/src/main/java/com/cicero/socialtools/InstaFragment.kt
@@ -2,4 +2,18 @@ package com.cicero.socialtools
 
 import androidx.fragment.app.Fragment
 
-class InstaFragment : Fragment(R.layout.fragment_insta)
+class InstaFragment : Fragment(R.layout.fragment_insta) {
+    private val flareTargets = listOf(
+        "respaskot",
+        "polresblitarofficial",
+        "polres_ponorogo",
+        "polreskediriofficial",
+        "ditlantaspoldajatim",
+        "humaspoldajatim",
+        "ditlantas_poldariau",
+        "ditlantaspoldajateng",
+        "divhumaspolri",
+        "divpropampolri",
+        "divtikpolri"
+    )
+}
