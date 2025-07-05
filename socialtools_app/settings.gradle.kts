4cb4b896397a558a6e55bd678f0b030d34c77e9a
papiqo <nationalite45@gmail.com>
2025-07-05 09:18:01 +0700

    Update FIx

--- socialtools_app/settings.gradle.kts
+++ socialtools_app/settings.gradle.kts
@@ -16,3 +16,11 @@ dependencyResolutionManagement {
 
 rootProject.name = "SocialToolsApp"
 include(":app")
+
+
+// settings.gradle
+include ":instagram4j"
+project(":instagram4j").projectDir = new File(rootDir, "external/instagram4j")include ":instagram4j"
+
+
+
