adfbde99dbd91fca5d47171ce57f5356279726e9
rizqa febryan prastyo <47109047+cicero78M@users.noreply.github.com>
2025-07-07 20:12:53 +0700

    Remove accessibility comment feature

--- socialtools_app/app/src/main/java/com/cicero/socialtools/ui/LandingActivity.kt
+++ socialtools_app/app/src/main/java/com/cicero/socialtools/ui/LandingActivity.kt
@@ -10,15 +10,6 @@ import java.io.File
 
 /** Simple landing page displayed on app launch. */
 class LandingActivity : AppCompatActivity() {
-    companion object {
-        const val ACTION_INPUT_COMMENT = "com.cicero.socialtools.INPUT_COMMENT"
-        const val EXTRA_COMMENT = "extra_comment"
-        const val ACTION_ACCESSIBILITY_LOG = "com.cicero.socialtools.ACCESS_LOG"
-        const val EXTRA_LOG_MESSAGE = "extra_log"
-        const val ACTION_COMMENT_RESULT = "com.cicero.socialtools.COMMENT_RESULT"
-        const val EXTRA_COMMENT_SUCCESS = "extra_success"
-        const val EXTRA_COMMENT_ERROR = "extra_error"
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
