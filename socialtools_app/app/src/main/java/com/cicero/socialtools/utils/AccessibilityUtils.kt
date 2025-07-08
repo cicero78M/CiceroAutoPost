1b21b29887cfcd44d1b0032a771e16edcacabf5a
rizqa febryan prastyo <47109047+cicero78M@users.noreply.github.com>
2025-07-08 18:25:28 +0700

    Revert "Fix accessibility service detection"

--- socialtools_app/app/src/main/java/com/cicero/socialtools/utils/AccessibilityUtils.kt
+++ socialtools_app/app/src/main/java/com/cicero/socialtools/utils/AccessibilityUtils.kt
@@ -3,7 +3,6 @@ package com.cicero.socialtools.utils
 import android.accessibilityservice.AccessibilityService
 import android.accessibilityservice.AccessibilityServiceInfo
 import android.content.Context
-import android.content.ComponentName
 import android.view.accessibility.AccessibilityManager
 
 /** Utility for checking accessibility service status. */
@@ -13,13 +12,10 @@ object AccessibilityUtils {
      */
     fun isServiceEnabled(context: Context, serviceClass: Class<out AccessibilityService>): Boolean {
         val am = context.getSystemService(Context.ACCESSIBILITY_SERVICE) as AccessibilityManager
-        val enabled =
-            am.getEnabledAccessibilityServiceList(AccessibilityServiceInfo.FEEDBACK_ALL_MASK)
-        val expected = ComponentName(context, serviceClass)
+        val enabled = am.getEnabledAccessibilityServiceList(AccessibilityServiceInfo.FEEDBACK_ALL_MASK)
         for (service in enabled) {
             val info = service.resolveInfo.serviceInfo
-            val enabledName = ComponentName(info.packageName, info.name)
-            if (enabledName == expected) {
+            if (info.packageName == context.packageName && info.name == serviceClass.name) {
                 return true
             }
         }
